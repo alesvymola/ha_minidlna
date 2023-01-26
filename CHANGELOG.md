61338383eb1f9c764d6722aea0db4eb4f8555390
ezhov <ezhov-ka@UR.RT.RU>
2023-01-27 00:53:30 +0900

    changed to my configs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,15 +3,3 @@
 ## 0.1
 
 - First version
-
-## 0.2
-
-- Fix repository link
-
-## 0.3
-
-- Add options
-
-## 0.4
-
-- Fix config file for devices
--- run.sh
+++ run.sh
@@ -13,6 +13,8 @@ ingress_interface=$(bashio::addon.ip_address)
 ingress_entry=$(bashio::addon.ingress_entry)
 
 MEDIA_DIR="$(bashio::config 'media_dir')"
+FRIENDLY_NAME="$(bashio::config 'friendly_name')"
+DB_DIR="$(bashio::config 'db_dir')"
 dirlist=$(echo $MEDIA_DIR | tr ";" "\n")
 
 for dir in $dirlist
@@ -20,9 +22,17 @@ do
     echo "> setting media dir: [media_dir=$dir]"
 	sed -i "/XXXmedia_dirXXX/a \media_dir=$dir" /etc/minidlna.conf
 done
+
+echo "> setting friendly name: [friendly_name=$FRIENDLY_NAME]"
+sed -i "s/%%friendly_name%%/${FRIENDLY_NAME}}/g" /etc/minidlna.conf
+
+echo "> setting db dir: [db_dir=$DB_DIR]"
+sed -i "s/%%db_dir%%/${DB_DIR}}/g" /etc/minidlna.conf
+
 #MEDIA_DIR2="$(bashio::config 'media_dir2')"
 #sed -i "s%XXXmedia_dir2XXX%$MEDIA_DIR2%g" /etc/minidlna.conf
 
+echo "> setting ingress port: [port=$ingress_port]"
 sed -i "s/%%port%%/${ingress_port}/g" /etc/minidlna.conf
 
 OPTIONS="$(bashio::config 'options')"
